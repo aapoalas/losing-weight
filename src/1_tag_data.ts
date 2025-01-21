2abeb30383695a8a1d98731d4d3fafa9cc0930ad
Aapo Alasuutari <aapo.alasuutari@gmail.com>
2025-01-21 21:00:02 +0200

    Restart

---
+++ src/1_tag_data.ts
@@ -0,0 +1,26 @@
+type LoopLookupTable = Map<string, LoopData>;
+
+interface LoopData {
+  loopName: string;
+  componentTypes: string[];
+  componentObjects: ComponentObject[];
+  faceplateTypes: string[];
+  faceplateObjects: ComponentObject[];
+  source: SourceKind;
+  loopMetadata: LoopMetadataObject;
+}
+
+type SourceKind = -1 | 1 | 2;
+
+interface ComponentObject {
+  kind: "component";
+  type: string;
+  loopName: string;
+  loopAdapter: string;
+  [componentConfigurationProperty: string]: unknown;
+}
+
+interface LoopMetadataObject {
+  loopName: string;
+  [loopMetadataProperty: string]: unknown;
+}
